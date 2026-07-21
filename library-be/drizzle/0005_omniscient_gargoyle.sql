e1ecf0907b430372ca6e7da6697ccfbaf784b085
janfdev <fauzanrizqinoor@gmail.com>
2026-07-22 02:51:16 +0700

    Remove contentType/mediaType/carrierType/frequency columns, consolidate into gmd

--- library-be/drizzle/0005_omniscient_gargoyle.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "bibliographies" ADD COLUMN "content_type" varchar(50);--> statement-breakpoint
-ALTER TABLE "bibliographies" ADD COLUMN "media_type" varchar(50);--> statement-breakpoint
-ALTER TABLE "bibliographies" ADD COLUMN "carrier_type" varchar(50);--> statement-breakpoint
-ALTER TABLE "bibliographies" ADD COLUMN "frequency" varchar(50);
